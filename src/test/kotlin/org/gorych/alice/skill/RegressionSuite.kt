e787c2054ec9d7841173909edecf25fa2494045b
Yahor Semianchenia <gorych911@gmail.com>
2026-02-15 16:10:34 +0300

    Added RegressionSuite

---
+++ src/test/kotlin/org/gorych/alice/skill/RegressionSuite.kt
@@ -0,0 +1,8 @@
+package org.gorych.alice.skill
+
+import org.junit.platform.suite.api.SelectPackages
+import org.junit.platform.suite.api.Suite
+
+@Suite
+@SelectPackages("org.gorych.alice.skill")
+class RegressionSuite
