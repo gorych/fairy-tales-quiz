5a4aeb268d787340a14dddc87f32ba262b191c75
Yahor Semianchenia <gorych911@gmail.com>
2026-02-18 15:28:53 +0300

    UserInteractionTest update

--- src/test/kotlin/org/gorych/alice/skill/fairytail/UserInteractionTest.kt
+++ src/test/kotlin/org/gorych/alice/skill/fairytail/UserInteractionTest.kt
@@ -6,11 +6,12 @@ import io.kotest.assertions.json.shouldEqualSpecifiedJson
 import org.gorych.alice.skill.util.readJsonResourceFile
 import tools.jackson.module.kotlin.jacksonObjectMapper
 import kotlin.test.Test
+import kotlin.test.assertFalse
 import kotlin.test.assertTrue
 
 private const val TEST_RESOURCES_DIR = "user_interaction_positive_test"
 
-class UserInteractionPositiveTest {
+class UserInteractionTest {
 
     @Test
     fun `WHEN session is new THEN introduction text should be returned`() {
@@ -23,18 +24,46 @@ class UserInteractionPositiveTest {
     }
 
     @Test
+    fun `WHEN user sends'no' THEN regret phrase should be returned and session closed`() {
+        shouldEqualJsonTest("step2-no.json")
+    }
+
+    @Test
     fun `WHEN user sends right answer to question #1 THEN second question should be returned`() {
         shouldEqualSpecifiedJsonAndContainText(
             fileName = "step3-answer1.json",
-            expectedText = "Кто съел бабушку красной шапочки?"
+            expectedText = arrayOf("Кто съел бабушку красной шапочки?", "следующий вопрос"),
         )
+    }
+
+    @Test
+    fun `WHEN user sends wrong answer to Q1 THEN 'think again' phrase should be returned and session state must be kept`() {
+        //given
+        val inputJson = readInputFile("step3-wrong-answer1.json")
+        val expectedJson = readOutputFile("step3-wrong-answer1.json")
+
+        //when
+        val actualJson = handle(inputJson)
+
+        //then
+        actualJson.shouldEqualSpecifiedJson(expectedJson)
+        actualJson.shouldContainJsonKey("$.response.text")
+
+        val responseText = jacksonObjectMapper()
+            .readTree(actualJson).get("response").get("text")
+            .toString()
+
+        val hastLeastOneMatchingText: Boolean = arrayOf("подумай", "попытайся", "попробуй")
+            .map { responseText.contains(it, ignoreCase = true) }
+            .any { it }
+        assertTrue(hastLeastOneMatchingText)
     }
 
     @Test
     fun `WHEN user sends right answer to question #2 THEN third question should be returned`() {
         shouldEqualSpecifiedJsonAndContainText(
             fileName = "step4-answer2.json",
-            expectedText = "Кто испёк колобка?"
+            expectedText = arrayOf("Кто испёк колобка?", "следующий вопрос"),
         )
     }
 
@@ -42,7 +71,7 @@ class UserInteractionPositiveTest {
     fun `WHEN user sends right answer to question #3 THEN fourth question should be returned`() {
         shouldEqualSpecifiedJsonAndContainText(
             fileName = "step5-answer3.json",
-            expectedText = "Как звали курочку, которая снесла золотое яичко?"
+            expectedText = arrayOf("Как звали курочку, которая снесла золотое яичко?", "следующий вопрос"),
         )
     }
 
@@ -50,7 +79,7 @@ class UserInteractionPositiveTest {
     fun `WHEN user sends right answer to question #4 THEN fifth question should be returned`() {
         shouldEqualSpecifiedJsonAndContainText(
             fileName = "step6-answer4.json",
-            expectedText = "Как звали собаку в сказке репка?"
+            expectedText = arrayOf("Как звали собаку в сказке репка?", "следующий вопрос"),
         )
     }
 
@@ -71,7 +100,7 @@ class UserInteractionPositiveTest {
         expectedJson.shouldEqualJson(actualJson)
     }
 
-    private fun shouldEqualSpecifiedJsonAndContainText(fileName: String, expectedText: String) {
+    private fun shouldEqualSpecifiedJsonAndContainText(fileName: String, vararg expectedText: String) {
         //given
         val inputJson = readInputFile(fileName)
         val expectedJson = readOutputFile(fileName)
@@ -86,10 +115,11 @@ class UserInteractionPositiveTest {
         val responseText = jacksonObjectMapper()
             .readTree(actualJson).get("response").get("text")
             .toString()
-        assertTrue {
-            responseText.contains("следующий вопрос", ignoreCase = true)
-            responseText.contains(expectedText)
-        }
+
+        val hasNotMatchingText: Boolean = expectedText
+            .map { responseText.contains(it, ignoreCase = true) }
+            .any { !it }
+        assertFalse(hasNotMatchingText)
     }
 
     private fun readInputFile(fileName: String) =
