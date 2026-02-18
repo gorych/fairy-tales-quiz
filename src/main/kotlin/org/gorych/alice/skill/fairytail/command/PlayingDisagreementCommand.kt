b8416a15e880da7f0f69f04a4edf2b10b6941b27
Yahor Semianchenia <gorych911@gmail.com>
2026-02-18 11:16:07 +0300

    Implemented PlayingDisagreementCommand

---
+++ src/main/kotlin/org/gorych/alice/skill/fairytail/command/PlayingDisagreementCommand.kt
@@ -0,0 +1,31 @@
+package org.gorych.alice.skill.fairytail.command
+
+import org.gorych.alice.skill.core.api.RequestObject
+import org.gorych.alice.skill.core.api.ResponseObject
+import org.gorych.alice.skill.core.command.Command
+
+private const val DISAGREEMENT_INTENT_ID = "g911.disagreement"
+
+class PlayingDisagreementCommand : Command {
+
+    override fun name() = PlayingDisagreementCommand.name()
+
+    override fun canHandle(requestObject: RequestObject): Boolean {
+        return requestObject.containsIntent(DISAGREEMENT_INTENT_ID)
+    }
+
+    override fun execute(requestObject: RequestObject): ResponseObject {
+        if (requestObject.containsPlayingDisagreementCommand() && !requestObject.hasCurrentQuestion()) {
+            return ResponseObject.of(
+                text = "Жаль! А так хотелось поиграть. Если станет скучно, ты знаешь как меня найти",
+                endSession = true
+            )
+        }
+
+        return ResponseObject.ofUnclearCommand()
+    }
+
+    companion object {
+        fun name(): String = PlayingDisagreementCommand::class.java.simpleName
+    }
+}
